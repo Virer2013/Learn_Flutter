971ab0454a7753977538cbffb5909521e925a413
konstantin <Virer2007@mail.ru>
2020-03-12 16:38:15 +0300

    first commit

---
+++ layout_interface/lib/main.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Weather',
+            style: TextStyle(color: Colors.black87),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          iconTheme: IconThemeData(color: Colors.black54),
+          brightness: Brightness.light,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: _buildBody(),
+      ),
+    );
+  }
+}
+
+Widget _buildBody() {
+  return SingleChildScrollView(
+    child: Column(
+      children: <Widget>[
+        _headerImage(),
+        SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                _weatherDescription(),
+                Divider(),
+                _temperature(),
+                Divider(),
+                _temperatureForecast(),
+                Divider(),
+                _footerRatings(),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Image _headerImage() {
+  return Image(
+    image: NetworkImage(
+        'https://www.atlantawomensnetwork.org/wp-content/uploads/2018/10/clear-instincts.jpg'),
+    fit: BoxFit.cover,
+  );
+}
+
+Column _weatherDescription() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Text(
+        'Tuesday - May 22',
+        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      ),
+      Divider(),
+      Text(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio ligula, sagittis ut mi vel, tincidunt porttitor urna. Proin eu pretium diam. Curabitur gravida diam volutpat, fermentum nunc nec, accumsan odio.',
+        style: TextStyle(color: Colors.black54),
+      ),
+    ],
+  );
+}
+
+Row _temperature() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+            Icons.wb_sunny,
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+      SizedBox(width: 16.0),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text(
+                '15 Clear',
+                style: TextStyle(color: Colors.deepPurple),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text(
+                'Murmanskaya oblast, Murmans',
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+Wrap _temperatureForecast() {
+  return Wrap(
+    spacing: 10.0,
+    children: List.generate(8, (int index) {
+      return Chip(
+        label: Text(
+          '${index + 20}C',
+          style: TextStyle(fontSize: 15.0),
+        ),
+        avatar: Icon(
+          Icons.wb_cloudy,
+          color: Colors.blue.shade300,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4.0),
+          side: BorderSide(color: Colors.grey),
+        ),
+        backgroundColor: Colors.grey.shade100,
+      );
+    }),
+  );
+}
+
+Row _footerRatings() {
+  var stars = Row(
+    mainAxisSize: MainAxisSize.min,
+    children: <Widget>[
+      Icon(Icons.star, size: 15.0, color: Colors.yellow[600]),
+      Icon(Icons.star, size: 15.0, color: Colors.yellow[600]),
+      Icon(Icons.star, size: 15.0, color: Colors.yellow[600]),
+      Icon(Icons.star, size: 15.0, color: Colors.black),
+      Icon(Icons.star, size: 15.0, color: Colors.black),
+    ],
+  );
+
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: <Widget>[
+      Text(
+        'Info with openweathermap.org',
+        style: TextStyle(fontSize: 15.0),
+      ),
+      stars,
+    ],
+  );
+}
