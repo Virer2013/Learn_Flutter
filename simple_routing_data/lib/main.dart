19871e144ff1f028f779ecefb20608f711a1182e
konstantin <Virer2007@mail.ru>
2020-03-16 12:13:33 +0300

    first commit

---
+++ simple_routing_data/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: FirstHome(),
+  ));
+}
+
+class FirstHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Home'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {},
+          child: Text('Second Home'),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () {
+          User user = User(name: 'Konstantin', age: 34);
+          Route route = MaterialPageRoute(builder: (context) => SecondHome(user: user));
+          Navigator.push(context, route);
+        },
+        child: Text('Move to Page 2'),
+      ),
+    );
+  }
+}
+
+class SecondHome extends StatelessWidget {
+  final User user;
+  SecondHome({this.user});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${this.user.name} - ${this.user.age}'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go Back'),
+        ),
+      ),
+    );
+  }
+}
+
+class User {
+  final String name;
+  final int age;
+
+  User({this.name, this.age});
+}
