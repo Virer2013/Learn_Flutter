3e6a9b6eb135949e485afc2724b896e17ad9de4c
konstantin <Virer2007@mail.ru>
2020-03-16 11:34:44 +0300

    first commit

---
+++ network_http/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter & HTTP',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter & HTTP'),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
+
+Future<http.Response> getData() async {
+  const url = 'https://about.google/static/data/locations.json';
+  return await http.get(url);
+}
+
+void loadData() {
+  getData().then((response) {
+    if(response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
+  }).catchError((error) {
+    debugPrint(error.toString());
+  });
+}
