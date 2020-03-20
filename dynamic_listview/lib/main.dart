b92a21d55678a4851854c8ac881d4f21f094eee4
Konstantin <Virer2007@mail.ru>
2020-03-20 07:12:39 +0300

    first commit

--- dynamic_listview/lib/main.dart
+++ dynamic_listview/lib/main.dart
@@ -26,52 +26,18 @@ class BodyListView extends StatelessWidget {
 }
 
 Widget _myListView() {
-  final List<ListItem> items = List<ListItem>.generate(
-      10000,
-      (i) => i % 6 == 0
-          ? HeadingItem('Heading $i')
-          : MessageItem('Sender $i', 'Message body $i'));
+  final List<String> items = List<String>.generate(10000, (i) => 'Item $i');
 
   return ListView.builder(
     itemCount: items.length,
     itemBuilder: (context, index) {
-      final item = items[index];
-
-      if (item is HeadingItem) {
-        return ListTile(
-          title:
-              Text(item.heading, style: Theme.of(context).textTheme.headline),
-        );
-      } else if (item is MessageItem) {
-        return ListTile(
-          title: Text(item.sender),
-          subtitle: Text(item.body),
+      return Card(
+        child: ListTile(
+          title: Text('${items[index]}'),
           leading: Icon(Icons.insert_photo, color: Colors.red),
           trailing: Icon(Icons.keyboard_arrow_right),
-        );
-      }
-      return null;
-      // return Card(
-      //   child: ListTile(
-      //     title: Text('${items[index]}'),
-      //     leading: Icon(Icons.insert_photo, color: Colors.red),
-      //     trailing: Icon(Icons.keyboard_arrow_right),
-      //   ),
-      // );
+        ),
+      );
     },
   );
-}
-
-abstract class ListItem {}
-
-class HeadingItem implements ListItem {
-  final String heading;
-  HeadingItem(this.heading);
-}
-
-class MessageItem implements ListItem {
-  final String sender;
-  final String body;
-
-  MessageItem(this.sender, this.body);
 }
