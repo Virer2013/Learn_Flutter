cb2cace66df28e40e7b2ac0bef6e8d973f118706
Konstantin <Virer2007@mail.ru>
2020-04-09 17:19:54 +0300

    First commit

---
+++ provider_start_example/lib/main.dart
@@ -0,0 +1,187 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Inherited Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: DefaultTabController(
+        length: 3,
+        child: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Provider Demo"),
+              centerTitle: true,
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(icon: Icon(Icons.add)),
+                  Tab(icon: Icon(Icons.person)),
+                  Tab(icon: Icon(Icons.message)),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: <Widget>[
+                MyCountPage(),
+                MyUserPage(),
+                MyEventPage(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyCountPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('ChangeNotifierProvider Example',
+                style: TextStyle(fontSize: 20)),
+            SizedBox(height: 50),
+            Text('0', style: Theme.of(context).textTheme.display1),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.remove),
+                  color: Colors.red,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  color: Colors.green,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyUserPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Text('FutureProvider Example, users loaded from a File',
+              style: TextStyle(fontSize: 17)),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Container(
+                  height: 50,
+                  color: Colors.grey[(index * 200) % 400],
+                  child: Center(child: Text('TEST')));
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// Event page (counting)
+class MyEventPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('StreamProvider Example', style: TextStyle(fontSize: 20)),
+        SizedBox(height: 50),
+        Text('0', style: Theme.of(context).textTheme.display1)
+      ],
+    )));
+  }
+}
+
+// CountProvider (ChangeNotifier)
+class CountProvider extends ChangeNotifier {
+  int _count = 0;
+  int get counterValue => _count;
+
+  void _incrementCount() {
+    _count++;
+    notifyListeners();
+  }
+
+  void _decrementCount() {
+    _count--;
+    notifyListeners();
+  }
+}
+
+// UserProvider (Future)
+class UserProvider {
+  final String _dataPath = "assets/users.json";
+  List<User> users;
+
+  Future<String> loadAsset() async {
+    return await Future.delayed(Duration(seconds: 10), () async {
+      return await rootBundle.loadString(_dataPath);
+    });
+  }
+
+  Future<List<User>> loadUserData() async {
+    var dataString = await loadAsset();
+    Map<String, dynamic> jsonUserData = jsonDecode(dataString);
+    users = UserList.fromJson(jsonUserData['users']).users;
+    print('done loading user!' + jsonEncode(users));
+    return users;
+  }
+}
+
+// EventProvider (Stream)
+class EventProvider {
+  Stream<int> intStream() {
+    Duration interval = Duration(seconds: 2);
+    return Stream<int>.periodic(interval, (int _count) => _count++);
+  }
+}
+
+// User Model
+class User {
+  final String firstName, lastName, website;
+  const User(this.firstName, this.lastName, this.website);
+
+  User.fromJson(Map<String, dynamic> json)
+      : this.firstName = json['first_name'],
+        this.lastName = json['last_name'],
+        this.website = json['website'];
+}
+
+// User List Model
+class UserList {
+  final List<User> users;
+  UserList(this.users);
+
+  UserList.fromJson(List<dynamic> usersJson)
+      : users = usersJson.map((user) => User.fromJson(user)).toList();
+}
+
---
+++ scoped_model_start_example/lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ScopedModel Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'ScopedModel Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ScopedModel Widget'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          _AppRootWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class _AppRootWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4.0,
+      child: Column(
+        children: <Widget>[
+          Text('(Root Widget)', style: Theme.of(context).textTheme.display1),
+          SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _Counter(),
+              _Counter(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Counter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(4.0).copyWith(bottom: 32.0),
+      color: Colors.yellowAccent,
+      child: Column(
+        children: <Widget>[
+          Text('(Child Widget)'),
+          Text('0', style: Theme.of(context).textTheme.display1),
+          ButtonBar(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.remove),
+                color: Colors.red,
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.add),
+                color: Colors.green,
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
