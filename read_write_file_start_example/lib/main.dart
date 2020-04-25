a1f509e4ac491ef0a974df4e64f6dcd0695910e3
Konstantin <Virer2007@mail.ru>
2020-04-25 06:24:30 +0300

    First commit

---
+++ read_write_file_start_example/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Local file read/write Demo',
+      home: ReadWriteFileExample(),
+    );
+  }
+}
+
+class ReadWriteFileExample extends StatefulWidget {
+  @override
+  _ReadWriteFileExampleState createState() => _ReadWriteFileExampleState();
+}
+
+class _ReadWriteFileExampleState extends State<ReadWriteFileExample> {
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    FocusNode textFieldFocusNode = FocusNode();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Local file read/write Demo'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(20.0),
+        children: <Widget>[
+          Text('Write to local file:', style: TextStyle(fontSize: 20)),
+          TextField(
+            focusNode: textFieldFocusNode,
+            controller: _textController,
+            maxLines: null,
+            style: TextStyle(fontSize: 20)
+          ),
+          ButtonBar(
+            children: <Widget>[
+              MaterialButton(
+                child: Text('Load', style: TextStyle(fontSize: 20)),
+                onPressed: () {},
+              ),
+              MaterialButton(
+                child: Text('Save', style: TextStyle(fontSize: 20)),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          Divider(height: 20.0),
+          Text('Local file path:', style: Theme.of(context).textTheme.title),
+          Text('demo_localfile.txt', style: Theme.of(context).textTheme.subhead),
+          Divider(height: 20.0),
+          Text('Local file content:', style: Theme.of(context).textTheme.title),
+          Text('Content', style: Theme.of(context).textTheme.subhead),
+        ],
+      ),
+    );
+  }
+}
