d2650aebf013e8dd0dbbabc564323a29ae972706
Konstantin <Virer2007@mail.ru>
2021-05-14 15:11:49 +0300

    first commit

---
+++ rick_and_morty/lib/core/platform/network_info.dart
@@ -0,0 +1,15 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImp implements NetworkInfo {
+  final InternetConnectionChecker connectionChecker;
+
+  NetworkInfoImp(this.connectionChecker);
+  
+  @override
+  Future<bool> get isConnected => connectionChecker.hasConnection;
+
+}
